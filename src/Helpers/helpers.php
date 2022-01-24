af2a5b23f7c32157572f57e207ea75e16daff478
Yung Teck Sheng <yungts97@gmail.com>
2022-01-24 17:12:41 +0800

    🚧 working on helper function

---
+++ src/Helpers/helpers.php
@@ -0,0 +1,37 @@
+<?php
+if (! function_exists('is_year')) {
+    /**
+     * Create a collection from the given value.
+     *
+     * @param  mixed  $value
+     * @return \Illuminate\Support\Collection
+     */
+    function collect($value = null)
+    {
+        return new Collection($value);
+    }
+}
+if (! function_exists('is_date')) {
+    /**
+     * Create a collection from the given value.
+     *
+     * @param  mixed  $value
+     * @return \Illuminate\Support\Collection
+     */
+    function collect($value = null)
+    {
+        return new Collection($value);
+    }
+}
+if (! function_exists('is_month')) {
+    /**
+     * Create a collection from the given value.
+     *
+     * @param  mixed  $value
+     * @return \Illuminate\Support\Collection
+     */
+    function collect($value = null)
+    {
+        return new Collection($value);
+    }
+}
