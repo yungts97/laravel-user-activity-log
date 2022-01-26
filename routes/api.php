07510dd0140c5aa2b6be9d9004191c9c92b4b626
Yung Teck Sheng <yungts97@gmail.com>
2022-01-26 11:20:46 +0800

    🚸 add new api route for delete

--- routes/api.php
+++ routes/api.php
@@ -7,5 +7,6 @@ Route::prefix('api')->middleware(config('user-activity-log.middleware'))->group(
         Route::get('/filter-options', [UserActivityLogController::class, 'getFilterOption']);
         Route::get('/', [UserActivityLogController::class, 'index']);
         Route::get('/{log}', [UserActivityLogController::class, 'show']);
+        Route::delete('/', [UserActivityLogController::class, 'destroy']);
     });
 });
